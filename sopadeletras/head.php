fe2cc566ed54d83d29c7bef25cc42aeba8a63772
ledwarsan <ledwarsand@gmail.com>
2021-09-09 02:41:10 -0500

    ak va los estilos de sopa de letras

---
+++ sopadeletras/head.php
@@ -0,0 +1,17 @@
+<head>
+        <meta charset="utf-8">
+        <title>Sopa de letras RED</title>
+		<link href="css/pushy-buttons.css" rel="stylesheet">
+        <link rel="stylesheet" href="css/estilos.css">
+        <link rel="stylesheet" href="css/condicion.css">
+		<style>
+@import url('https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;700&display=swap');
+</style>
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<link href="css/bootstrap.css" rel="stylesheet" type="text/css">
+<script src="js/jquery-3.3.1.min.js"></script>
+<script src="js/popper.min.js"></script>
+<script src="js/bootstrap-4.3.1.js"></script>
+	
+	</head>
